75aac4b47bbe6168656e2464ea00dc0cd364b068
sanderdebr <sander606@gmail.com>
2020-02-03 09:25:55 +0100

    f

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db.js');
 
 // Load env variables
 dotenv.config({ path: './config/config.env' });
